8b46275b82db2995573eaeb54412d5a7743aba99
nicklasjepsen <nicklas.m.jepsen@gmail.com>
2016-01-28 22:50:59 +0100

    Initial commit

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using SystemOut.SmartInspect.Extensions;
+using Gurock.SmartInspect;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        private static string host = "";
+        private static string from = "";
+        private static string to = "";
+        private static int port;
+
+        static void Main(string[] args)
+        {
+            ProtocolFactory.RegisterProtocol("mail", typeof(MailProtocol));
+            var builder = new ConnectionsBuilder();
+            builder.BeginProtocol("mail");
+            builder.AddOption("host", host);
+            builder.AddOption("to", to);
+            builder.AddOption("from", from);
+            builder.AddOption("port", port);
+            builder.EndProtocol();
+            
+            SiAuto.Si.Connections = builder.Connections;
+            SiAuto.Si.Enabled = true;
+
+            SiAuto.Main.LogMessage("Debug", Level.Debug);
+            SiAuto.Main.LogMessage("Error", Level.Error);
+            SiAuto.Main.LogMessage("Fatal", Level.Fatal);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SmartInspect.MailProtocol/Class1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Net.Mail;
+using Gurock.SmartInspect;
+
+namespace SystemOut.SmartInspect.Extensions
+{
+    public class MailProtocol : Protocol
+    {
+        private SmtpClient smtpClient;
+        private string host;
+        private int port;
+        private string from;
+        private string to;
+
+        protected override void InternalConnect()
+        {
+            smtpClient = new SmtpClient(host, port);
+        }
+
+        protected override void InternalDisconnect()
+        {
+            smtpClient.Dispose();
+        }
+
+        private MailMessage FormatLogEntry(LogEntry logEntry)
+        {
+            return new MailMessage(from, to)
+            {
+                Subject = logEntry.Title,
+                Body = $"{logEntry.Level}{Environment.NewLine}{logEntry.Title}"
+            };
+        }
+
+        protected override void InternalWritePacket(Packet packet)
+        {
+            switch (packet.PacketType)
+            {
+                case PacketType.LogEntry:
+                    {
+                        var logEntry = packet as LogEntry;
+                        // TODO: I think there is a bug in SI - level is always set to Message
+                        // Only send if level is severe than error and logEntry is not null
+                        //if (logEntry?.Level > Level.Error)
+                        //{
+                        var message = FormatLogEntry(logEntry);
+                        smtpClient.Send(message);
+                        break;
+                    }
+            }
+        }
+
+        protected override bool IsValidOption(string name)
+        {
+            switch (name)
+            {
+                case "host":
+                    return true;
+                case "port":
+                    return true;
+                case "to":
+                    return true;
+                case "from":
+                    return true;
+                default:
+                    return base.IsValidOption(name);
+            }
+        }
+
+        protected override void LoadOptions()
+        {
+            base.LoadOptions();
+            host = GetStringOption("host", null);
+            port = GetIntegerOption("port", 25);
+            to = GetStringOption("to", null);
+            from = GetStringOption("from", null);
+        }
+
+        protected override string Name => "mail";
+    }
+}
